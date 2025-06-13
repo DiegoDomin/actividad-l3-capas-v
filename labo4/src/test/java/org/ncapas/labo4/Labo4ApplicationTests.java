38806f6e3c0886c5caaa2b555267a0f2d4841f25
Saenz <00164722@uca.edu.sv>
2025-06-13 07:30:03 -0600

    feat:estructura labo

---
+++ labo4/src/test/java/org/ncapas/labo4/Labo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ncapas.labo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
